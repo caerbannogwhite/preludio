4546f3c1c3f172273291a0af16dffd2ea56e4362
caerbannogwhite <massimo.meneghello93@gmail.com>
2022-08-20 17:02:55 +0100

    Run PRQL command run on selection code.

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import runPrqljs from './prqljs/prql.js';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "prqlvs" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('prqlvs.RunPRQL', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Run PRQL!');
-	});
-	context.subscriptions.push(disposable);
+	let disposable = vscode.commands.registerCommand('prqlvs.Run', () => {
+		const textEditor = vscode.window.activeTextEditor;
+		if (textEditor) {
 
-	let disp2 = vscode.commands.registerCommand('prqlvs.HelloPRQL', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from PRQL!');
+			const endOfLine = '\n';
+			const firstLine = textEditor.selection.start.line;
+			const lastLine = textEditor.selection.end.line;
+
+			let selectedText = textEditor.document.lineAt(firstLine).text.slice(textEditor.selection.start.character) + endOfLine;
+			for (let i = firstLine + 1; i < lastLine; ++i) {
+				selectedText += textEditor.document.lineAt(i).text;
+			}
+			selectedText += textEditor.document.lineAt(lastLine).text.slice(0, textEditor.selection.end.character) + endOfLine;
+			
+			console.log("<begin>");
+			console.log(selectedText);
+			console.log("<end>");
+
+			runPrqljs(selectedText);
+		}
+
+		vscode.window.showInformationMessage('PRQL code succesfully executed.');
 	});
-	context.subscriptions.push(disp2);
+
+	context.subscriptions.push(disposable);
 
 }
 
