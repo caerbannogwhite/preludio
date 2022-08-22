1a7bc19134b9d4fe68b8ac9f4974c257f67fafa1
caerbannogwhite <massimo.meneghello93@gmail.com>
2022-08-22 23:51:04 +0100

    JS listener code updated.

--- src/grammar/prqlListener.js
+++ src/grammar/prqlListener.js
@@ -266,15 +266,6 @@ export default class prqlListener extends antlr4.tree.ParseTreeListener {
 	}
 
 
-	// Enter a parse tree produced by prqlParser#number.
-	enterNumber(ctx) {
-	}
-
-	// Exit a parse tree produced by prqlParser#number.
-	exitNumber(ctx) {
-	}
-
-
 	// Enter a parse tree produced by prqlParser#range.
 	enterRange(ctx) {
 	}
