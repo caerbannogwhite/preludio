27dc73c1ba842e5fd86349f48f132e7e96dc5c3d
caerbannogwhite <massimo.meneghello93@gmail.com>
2022-10-30 22:18:39 +0000

    [test] python test code updated.

--- read_bytecode_test.py
+++ read_bytecode_test.py
@@ -1,26 +1,36 @@
 def read_bytecode(bytes_):
     bytemark = bytes_[0:4]
     table_len = int.from_bytes(bytes_[8:16], byteorder="big")
-    print("mark:", bytemark, "symbols:", table_len)
+    
+    print("BYTE MARK: ", bytemark)
+    print("SYMBOL NUM:", table_len, "\n")
+
+    print("TABLE")
+    print("-----")
     offset = 16
     symbols = []
-
     for _ in range(table_len):
         l = int.from_bytes(bytes_[offset:offset+8], byteorder="big")
         offset += 8
         symb = bytes_[offset:offset+l]
         offset += l
         symbols.append(symb)
-    
+        print(symb)
+    print()
+
+    print("OPERATIONS")
+    print("----------")
+    operations = []
     while offset < len(bytes_):
         ops = []
         for _ in range(3):
             ops.append(int.from_bytes(
                 bytes_[offset:offset+8], byteorder="big"))
             offset += 8
-        print(ops[0], ops[1], ops[2])
-    
-    return symbols
+        operations.append(ops)
+        print(f"{ops[0]:4} {ops[1]:4} {ops[2]:4}")
+
+    return symbols, operations
 
 
 def get_bytes(path):
