5764dca07060f82e0133b83e5b5f03cbabcf9a8b
caerbannogwhite <massimo.meneghello93@gmail.com>
2022-10-31 23:48:22 +0000

    [prql vm] Implementing read instruction.

--- prqlvm/src/main.rs
+++ prqlvm/src/main.rs
@@ -4,24 +4,28 @@ use std::collections::HashMap;
 use std::fs;
 use std::str;
 
-// const TYPE_NULL: u64 = 0;
-// const TYPE_BOOL: u64 = 1;
-// const TYPE_NUMERIC: u64 = 2;
-// const TYPE_STRING: u64 = 3;
-// const TYPE_IDENT: u64 = 4;
-// const TYPE_INTERVAL: u64 = 5;
-// const TYPE_RANGE: u64 = 6;
-// const TYPE_LIST: u64 = 7;
-// const TYPE_PIPELINE: u64 = 8;
+mod prql_std;
+mod prql_vm;
+
+const TYPE_NULL: u64 = 0;
+const TYPE_BOOL: u64 = 1;
+const TYPE_NUMERIC: u64 = 2;
+const TYPE_STRING: u64 = 3;
+const TYPE_IDENT: u64 = 4;
+const TYPE_INTERVAL: u64 = 5;
+const TYPE_RANGE: u64 = 6;
+const TYPE_LIST: u64 = 7;
+const TYPE_PIPELINE: u64 = 8;
 
 const OP_BEGIN_PIPELINE: u64 = 0;
 const OP_END_PIPELINE: u64 = 1;
 const OP_ASSIGN_TABLE: u64 = 2;
-const OP_BEGIN_LIST: u64 = 4;
-const OP_END_LIST: u64 = 5;
-const OP_ADD_FUNC_PARAM: u64 = 6;
-const OP_ADD_EXPR_TERM: u64 = 7;
-const OP_CALL_FUNC: u64 = 8;
+const OP_BEGIN_FUNC_CALL: u64 = 3;
+const OP_END_FUNC_CALL: u64 = 4;
+const OP_BEGIN_LIST: u64 = 5;
+const OP_END_LIST: u64 = 6;
+const OP_ADD_FUNC_PARAM: u64 = 7;
+const OP_ADD_EXPR_TERM: u64 = 8;
 const OP_PUSH_NAMED_PARAM: u64 = 9;
 const OP_PUSH_ASSIGN_IDENT: u64 = 10;
 const OP_PUSH_TERM: u64 = 11;
@@ -63,20 +67,20 @@ fn main() {
     let cli = Cli::parse();
 
     let mut vm = PRQLVirtualMachine::new();
-    // vm.__debug_level__ = cli.debug;
-    // vm.__verbosity_level__ = cli.verbosity;
-    // vm.input_file = cli.input_file;
+    vm.__debug_level = cli.debug;
+    vm.__verbosity_level = cli.verbosity;
+    vm.input_file = cli.input_file;
 
-    vm.__debug_level__ = 10;
-    vm.__verbosity_level__ = 10;
-    vm.input_file = String::from("C:\\Users\\massi\\source\\repos\\prqlvs\\bytecode");
+    // vm.__debug_level = 10;
+    // vm.__verbosity_level = 10;
+    // vm.input_file = String::from("C:\\Users\\massi\\source\\repos\\prqlvs\\bytecode");
 
-    if vm.__verbosity_level__ > 0 {
+    if vm.__verbosity_level > 0 {
         println!("PRQL VM");
         println!("-------");
-        println!("Debug level:     {}", vm.__debug_level__);
-        println!("Verbosity level: {}", vm.__verbosity_level__);
-        println!("Input file:      {}", vm.input_file);
+        println!("Debug level:     {}", vm.__debug_level);
+        println!("Verbosity level: {}", vm.__verbosity_level);
+        println!("Input file:      {}\n", vm.input_file);
     }
 
     let input = fs::read(&vm.input_file).unwrap();
@@ -84,35 +88,44 @@ fn main() {
 }
 
 pub struct PRQLVirtualMachine {
-    __verbosity_level__: u8,
-    __debug_level__: u8,
-    __counter__: u64,
-    __current_directory__: String,
-    __current_table__: DataFrame,
-    __symbol_table__: Vec<String>,
-    __functions__: HashMap<String, fn(&mut PRQLVirtualMachine)>,
-    __variables__: HashMap<String, DataFrame>,
+    __verbosity_level: u8,
+    __debug_level: u8,
+    __counter: u64,
+    __current_directory: String,
+    __current_table: DataFrame,
+    __symbol_table: Vec<String>,
+    __functions: HashMap<String, fn(&mut PRQLVirtualMachine)>,
+    __variables: HashMap<String, DataFrame>,
 
     input_file: String,
 }
 
 impl PRQLVirtualMachine {
     pub fn new() -> PRQLVirtualMachine {
-        return PRQLVirtualMachine {
-            __verbosity_level__: 0,
-            __debug_level__: 0,
-            __counter__: 0,
-            __current_directory__: String::new(),
-            __current_table__: DataFrame::empty(),
-            __symbol_table__: Vec::new(),
-            __functions__: HashMap::new(),
-            __variables__: HashMap::new(),
+        let mut vm = PRQLVirtualMachine {
+            __verbosity_level: 0,
+            __debug_level: 0,
+            __counter: 0,
+            __current_directory: String::new(),
+            __current_table: DataFrame::empty(),
+            __symbol_table: Vec::new(),
+            __functions: HashMap::new(),
+            __variables: HashMap::new(),
 
             input_file: String::new(),
         };
+
+        // Load functions
+        vm.__functions.insert(String::from("derive"), prql_derive);
+        vm.__functions.insert(String::from("from"), prql_from);
+        vm.__functions.insert(String::from("import"), prql_import);
+
+        return vm;
     }
 
     pub fn read_prql_bytecode(&mut self, bytes: &[u8]) {
+        /////////////////////////////////////////////////////////////
+        ////                    PREAMBLE
         // check signature
         if bytes[0] != 0x11 || bytes[1] != 0x01 || bytes[2] != 0x19 || bytes[3] != 0x93 {
             panic!("Wrong bytecode format.")
@@ -124,6 +137,8 @@ impl PRQLVirtualMachine {
             bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15],
         ];
 
+        /////////////////////////////////////////////////////////////
+        ////                    SYMBLE TABLE
         let table_length: u64 = u64::from_be_bytes(buff);
         let mut offset: u64 = 16;
         for _ in 0..table_length {
@@ -138,61 +153,157 @@ impl PRQLVirtualMachine {
             // read symbol and insert into the symbol table
             let res = str::from_utf8(&bytes[(offset as usize)..(offset + symbol_length) as usize])
                 .unwrap();
-            self.__symbol_table__.push(res.to_string());
+            self.__symbol_table.push(res.to_string());
 
             offset += symbol_length;
         }
 
-        if self.__debug_level__ > 5 {
+        if self.__debug_level > 5 {
+            println!("BYTECODE");
+            println!("--------");
             println!(
-                "BYTE MARK: {:x}{:x}{:x}{:x}",
+                "BYTE MARK:       0x{:x} 0x{:x} 0x{:x} 0x{:x}",
                 bytes[0], bytes[1], bytes[2], bytes[3]
             );
             println!("SYMBOL NUM: {}\n", table_length);
 
-            println!("TABLE");
-            println!("-----");
-            for symb in self.__symbol_table__.iter() {
+            println!("SYMBOLS");
+            println!("-------");
+            for symb in self.__symbol_table.iter() {
                 println!("{}", symb)
             }
+            println!();
+
+            println!("OPERATIONS");
+            println!("----------");
+        }
+
+        /////////////////////////////////////////////////////////////
+        ////                    INSTRUCTIONS
+        while (offset as usize) < bytes.len() {
+            for j in 0..8 {
+                buff[j] = bytes[(offset as usize) + j];
+            }
+            let op_code: u64 = u64::from_be_bytes(buff);
+            offset += 8;
+
+            for j in 0..8 {
+                buff[j] = bytes[(offset as usize) + j];
+            }
+            let param1: u64 = u64::from_be_bytes(buff);
+            offset += 8;
+
+            for j in 0..8 {
+                buff[j] = bytes[(offset as usize) + j];
+            }
+            let param2: u64 = u64::from_be_bytes(buff);
+            offset += 8;
+
+            self.read_instruction(op_code, param1, param2)
         }
     }
 
-    pub fn push_instruction(&self, op_code: u64, param1: u64, param2: u64) {
+    pub fn read_instruction(&self, op_code: u64, param1: u64, param2: u64) {
         match op_code {
             // PIPELINE
             OP_BEGIN_PIPELINE => {
-                if self.__verbosity_level__ > 10 {
-                    println!("OP_BEGIN_PIPELINE");
+                if self.__debug_level > 10 {
+                    println!("{:<20} | {:<20} | {:<20}", "OP_BEGIN_PIPELINE", "", "");
                 }
             }
 
             OP_END_PIPELINE => {
-                if self.__verbosity_level__ > 10 {
-                    println!("OP_END_PIPELINE");
+                if self.__debug_level > 10 {
+                    println!("{:<20} | {:<20} | {:<20}", "OP_END_PIPELINE", "", "");
                 }
             }
 
             OP_ASSIGN_TABLE => {}
+
+            OP_BEGIN_FUNC_CALL => {
+                let function_name = &self.__symbol_table[(param1 as usize)];
+                if self.__debug_level > 10 {
+                    println!(
+                        "{:<20} | {:<20} | {:<20}",
+                        "OP_BEGIN_FUNC_CALL", function_name, ""
+                    );
+                }
+            }
+
+            OP_END_FUNC_CALL => {
+                if self.__debug_level > 10 {
+                    println!("{:<20} | {:<20} | {:<20}", "OP_END_FUNC_CALL", "", "");
+                }
+            }
+
             OP_BEGIN_LIST => {}
             OP_END_LIST => {}
+
             OP_ADD_FUNC_PARAM => {}
             OP_ADD_EXPR_TERM => {}
-            OP_CALL_FUNC => {}
-            OP_PUSH_NAMED_PARAM => {}
+
+            OP_PUSH_NAMED_PARAM => {
+                if self.__debug_level > 10 {
+                    println!("{:<20} | {:<20} | {:<20}", "OP_PUSH_NAMED_PARAM", "", "");
+                }
+            }
+
             OP_PUSH_ASSIGN_IDENT => {}
-            OP_PUSH_TERM => {}
-            OP_END_FUNC_CALL_PARAM => {}
+
+            OP_PUSH_TERM => {
+                let mut term_type_str = "UNKNOWN";
+                let mut term_val = "";
+                match param1 {
+                    TYPE_NULL => {
+                        term_type_str = "NULL";
+                    }
+                    TYPE_BOOL => {
+                        term_type_str = "BOOL";
+                        if param2 == 1 {
+                            term_val = "true";
+                        } else {
+                            term_val = "false";
+                        };
+                    }
+                    TYPE_NUMERIC => {
+                        term_type_str = "NUMERIC";
+                        term_val = &self.__symbol_table[(param2 as usize)];
+                    }
+                    TYPE_STRING => {
+                        term_type_str = "STRING";
+                        term_val = &self.__symbol_table[(param2 as usize)];
+                    }
+                    TYPE_IDENT => {
+                        term_type_str = "IDENT";
+                        term_val = &self.__symbol_table[(param2 as usize)];
+                    }
+                    _ => {}
+                }
+
+                if self.__debug_level > 10 {
+                    println!(
+                        "{:<20} | {:<20} | {:<20}",
+                        "OP_PUSH_TERM", term_type_str, term_val
+                    );
+                }
+            }
+
+            OP_END_FUNC_CALL_PARAM => {
+                if self.__debug_level > 10 {
+                    println!("{:<20} | {:<20} | {:<20}", "OP_END_FUNC_CALL_PARAM", "", "");
+                }
+            }
+
             OP_GOTO => {}
             _ => println!("Unknown op code: {}", op_code),
         }
     }
 }
 
-fn prql_derive(vm: &mut PRQLVirtualMachine) {}
+pub fn prql_derive(vm: &mut PRQLVirtualMachine) {}
 
-fn prql_from(vm: &mut PRQLVirtualMachine) {}
+pub fn prql_from(vm: &mut PRQLVirtualMachine) {}
 
-fn prql_import(vm: &mut PRQLVirtualMachine) {
-    vm.__current_table__ = CsvReader::from_path("path.csv").unwrap().finish().unwrap();
+pub fn prql_import(vm: &mut PRQLVirtualMachine) {
+    vm.__current_table = CsvReader::from_path("path.csv").unwrap().finish().unwrap();
 }
---
+++ prqlvm/src/prql_std.rs
@@ -0,0 +1,7 @@
+// pub fn prql_derive(vm: &mut PRQLVirtualMachine) {}
+
+// pub fn prql_from(vm: &mut PRQLVirtualMachine) {}
+
+// pub fn prql_import(vm: &mut PRQLVirtualMachine) {
+//     vm.__current_table__ = CsvReader::from_path("path.csv").unwrap().finish().unwrap();
+// }
