02daed82d0f24f3ae8d07462f01adb6c70ce5c92
caerbannogwhite <massimo.meneghello93@gmail.com>
2022-11-06 18:51:07 +0000

    [prql vm] Multiplication implemented.

--- prqlvm/src/main.rs
+++ prqlvm/src/main.rs
@@ -93,6 +93,7 @@ pub struct Operation {
     opcode: u64,
     param1: u64,
     param2: u64,
+    num: f64,
 }
 
 pub struct PRQLVirtualMachine {
@@ -101,8 +102,7 @@ pub struct PRQLVirtualMachine {
     __counter: u64,
     __current_directory: String,
     __current_table: DataFrame,
-    __num_symbol_table: Vec<f64>,
-    __str_symbol_table: Vec<String>,
+    __symbol_table: Vec<String>,
     __stack: Vec<Operation>,
     __functions: HashMap<String, fn(&mut PRQLVirtualMachine)>,
     __variables: HashMap<String, DataFrame>,
@@ -120,8 +120,7 @@ impl PRQLVirtualMachine {
 
             __current_table: DataFrame::empty(),
 
-            __num_symbol_table: Vec::new(),
-            __str_symbol_table: Vec::new(),
+            __symbol_table: Vec::new(),
             __stack: Vec::new(),
             __functions: HashMap::new(),
             __variables: HashMap::new(),
@@ -153,27 +152,10 @@ impl PRQLVirtualMachine {
         ];
 
         /////////////////////////////////////////////////////////////
-        ////                    NUM SYMBOL TABLE
-        let num_table_length: u64 = u64::from_be_bytes(buff);
+        ////                    SYMBOL TABLE
+        let table_length: u64 = u64::from_be_bytes(buff);
         let mut offset: u64 = 16;
-        for _ in 0..num_table_length {
-            // copy length of string into the buffer
-            for j in 0..8 {
-                buff[j] = bytes[(offset as usize) + j];
-            }
-
-            self.__num_symbol_table.push(f64::from_ne_bytes(buff));
-            offset += 8;
-        }
-
-        /////////////////////////////////////////////////////////////
-        ////                    STR SYMBOL TABLE
-        for j in 0..8 {
-            buff[j] = bytes[(offset as usize) + j];
-        }
-        let str_table_length: u64 = u64::from_be_bytes(buff);
-        offset += 8;
-        for _ in 0..str_table_length {
+        for _ in 0..table_length {
             // copy length of string into the buffer
             for j in 0..8 {
                 buff[j] = bytes[(offset as usize) + j];
@@ -185,7 +167,7 @@ impl PRQLVirtualMachine {
             // read symbol and insert into the symbol table
             let res = str::from_utf8(&bytes[(offset as usize)..(offset + symbol_length) as usize])
                 .unwrap();
-            self.__str_symbol_table.push(res.to_string());
+            self.__symbol_table.push(res.to_string());
 
             offset += symbol_length;
         }
@@ -197,20 +179,11 @@ impl PRQLVirtualMachine {
                 "BYTE MARK:       0x{:x} 0x{:x} 0x{:x} 0x{:x}",
                 bytes[0], bytes[1], bytes[2], bytes[3]
             );
-            println!("NUMERIC SYMBOL NUM: {}\n", num_table_length);
-
-            println!("NUMERIC SYMBOLS");
-            println!("---------------");
-            for symb in self.__num_symbol_table.iter() {
-                println!("{}", symb)
-            }
-            println!();
-
-            println!("STRING SYMBOL NUM: {}\n", str_table_length);
+            println!("STRING SYMBOL NUM: {}\n", table_length);
 
             println!("STRING SYMBOLS");
             println!("--------------");
-            for symb in self.__str_symbol_table.iter() {
+            for symb in self.__symbol_table.iter() {
                 println!("{}", symb)
             }
             println!();
@@ -262,7 +235,7 @@ impl PRQLVirtualMachine {
             OP_ASSIGN_TABLE => {}
 
             OP_BEGIN_FUNC_CALL => {
-                let function_name = &self.__str_symbol_table[(param1 as usize)];
+                let function_name = &self.__symbol_table[(param1 as usize)];
                 if self.__debug_level > 10 {
                     println!(
                         "{:<25} | {:<20} | {:<20}",
@@ -309,15 +282,15 @@ impl PRQLVirtualMachine {
                         }
                         TYPE_NUMERIC => {
                             term_type_str = "NUMERIC";
-                            // term_val = &self.__num_symbol_table[(param2 as usize)].to_string();
+                            let tmp = Self::__param_to_float(param2).to_string();
                         }
                         TYPE_STRING => {
                             term_type_str = "STRING";
-                            term_val = &self.__str_symbol_table[(param2 as usize)];
+                            term_val = &self.__symbol_table[(param2 as usize)];
                         }
                         TYPE_IDENT => {
                             term_type_str = "IDENT";
-                            term_val = &self.__str_symbol_table[(param2 as usize)];
+                            term_val = &self.__symbol_table[(param2 as usize)];
                         }
                         _ => {}
                     }
@@ -328,10 +301,16 @@ impl PRQLVirtualMachine {
                     );
                 }
 
+                let mut num = 0.0;
+                if param1 == TYPE_NUMERIC {
+                    num = Self::__param_to_float(param2);
+                }
+
                 self.__stack.push(Operation {
                     opcode: opcode,
                     param1: param1,
                     param2: param2,
+                    num: num,
                 })
             }
 
@@ -343,6 +322,8 @@ impl PRQLVirtualMachine {
 
             OP_GOTO => {}
 
+            /////////////////////////////////////////////////////////
+            ////                MULTIPLICATION
             OP_BINARY_MUL => {
                 let term2 = self.__stack.pop().unwrap();
                 let term1 = self.__stack.pop().unwrap();
@@ -351,56 +332,99 @@ impl PRQLVirtualMachine {
                     opcode: OP_PUSH_TERM,
                     param1: 0,
                     param2: 0,
+                    num: 0.0,
                 };
 
                 match term1.param1 {
                     TYPE_NULL => match term2.param2 {
                         TYPE_NULL => result.param1 = TYPE_NULL,
-                        TYPE_BOOL => {
-                            result.param1 = TYPE_BOOL;
-                            result.param2 = 0;
+                        TYPE_BOOL => result.param1 = TYPE_BOOL,
+                        TYPE_NUMERIC => result.param1 = TYPE_NUMERIC,
+                        TYPE_STRING => {
+                            result.param1 = TYPE_STRING;
+                            result.param2 = self.__insert_symbol(String::from(""));
                         }
-                        TYPE_NUMERIC => {
-                            result.param1 = TYPE_NUMERIC;
-
-                            result.param2 = 0;
-                        }
-                        TYPE_STRING => result.param1 = TYPE_NULL,
-                        TYPE_IDENT => result.param1 = TYPE_NULL,
+                        TYPE_IDENT => {}
                         _ => {}
                     },
                     TYPE_BOOL => match term2.param2 {
-                        TYPE_NULL => result.param1 = TYPE_NULL,
+                        TYPE_NULL => result.param1 = TYPE_BOOL,
                         TYPE_BOOL => {
                             result.param1 = TYPE_BOOL;
                             result.param2 = term1.param2 * term2.param2;
                         }
-                        TYPE_NUMERIC => {}
-                        TYPE_STRING => {}
+                        TYPE_NUMERIC => {
+                            result.param1 = TYPE_NUMERIC;
+                            if term1.param2 == 1 {
+                                result.num = term2.num;
+                            } else {
+                                result.num = 0.0;
+                            }
+                        }
+                        TYPE_STRING => {
+                            result.param1 = TYPE_STRING;
+                            if term1.param2 == 1 {
+                                result.param2 = term2.param2;
+                            } else {
+                                result.param2 = self.__insert_symbol(String::from(""));
+                            }
+                        }
                         TYPE_IDENT => {}
                         _ => {}
                     },
                     TYPE_NUMERIC => match term2.param2 {
-                        TYPE_NULL => result.param1 = TYPE_NULL,
-                        TYPE_BOOL => {}
-                        TYPE_NUMERIC => {}
-                        TYPE_STRING => {}
+                        TYPE_NULL => {
+                            result.param1 = TYPE_NUMERIC;
+                            result.num = 0.0;
+                        }
+                        TYPE_BOOL => {
+                            result.param1 = TYPE_NUMERIC;
+                            if term2.param2 == 1 {
+                                result.num = term1.num;
+                            } else {
+                                result.num = 0.0;
+                            }
+                        }
+                        TYPE_NUMERIC => {
+                            result.param1 = TYPE_NUMERIC;
+                            result.num = term1.num * term2.num;
+                        }
+                        TYPE_STRING => {
+                            result.param1 = TYPE_STRING;
+                            result.param2 = self.__insert_symbol(
+                                self.__symbol_table[(term2.param2 as usize)]
+                                    .repeat(term1.num.to_usize().unwrap()),
+                            );
+                        }
                         TYPE_IDENT => {}
                         _ => {}
                     },
                     TYPE_STRING => match term2.param2 {
                         TYPE_NULL => {
                             result.param1 = TYPE_STRING;
-                            result.param2 = self.insert_string_symbol(String::from(""))
+                            result.param2 = self.__insert_symbol(String::from(""))
                         }
-                        TYPE_BOOL => {}
-                        TYPE_NUMERIC => {}
+                        TYPE_BOOL => {
+                            result.param1 = TYPE_STRING;
+                            if term2.param2 == 1 {
+                                result.param2 = term1.param2;
+                            } else {
+                                result.param2 = self.__insert_symbol(String::from(""));
+                            }
+                        }
+                        TYPE_NUMERIC => {
+                            result.param1 = TYPE_STRING;
+                            result.param2 = self.__insert_symbol(
+                                self.__symbol_table[(term1.param2 as usize)]
+                                    .repeat(term2.num.to_usize().unwrap()),
+                            );
+                        }
                         TYPE_STRING => {}
                         TYPE_IDENT => {}
                         _ => {}
                     },
                     TYPE_IDENT => match term2.param2 {
-                        TYPE_NULL => {},
+                        TYPE_NULL => {}
                         TYPE_BOOL => {}
                         TYPE_NUMERIC => {}
                         TYPE_STRING => {}
@@ -422,16 +446,25 @@ impl PRQLVirtualMachine {
         }
     }
 
-    fn insert_string_symbol(&mut self, symb: String) -> u64 {
-        let res = self.__str_symbol_table.iter().position(|r| r == &symb);
+    pub fn __float_to_param(f: f64) -> u64 {
+        let bytes = f.to_ne_bytes();
+        u64::from_be_bytes(bytes)
+    }
+
+    pub fn __param_to_float(p: u64) -> f64 {
+        let bytes = p.to_be_bytes();
+        f64::from_bits(u64::from_ne_bytes(bytes))
+    }
+
+    fn __insert_symbol(&mut self, symb: String) -> u64 {
+        let res = self.__symbol_table.iter().position(|r| r == &symb);
         if res.is_some() {
             return res.unwrap().to_u64().unwrap();
         }
 
-        let l = self.__str_symbol_table.len();
-        self.__str_symbol_table.push(symb);
+        let l = self.__symbol_table.len();
+        self.__symbol_table.push(symb);
         return l.to_u64().unwrap();
-
     }
 }
 
