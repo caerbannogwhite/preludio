9fbd5d7b14bb0255bd9f37026961e84b5dc8abae
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2022-11-17 23:38:08 +0000

    [prql vm] Type system moved into separate file.

--- prqlvm/src/main.rs
+++ prqlvm/src/main.rs
@@ -2,14 +2,13 @@ use clap::Parser;
 use polars::export::num::ToPrimitive;
 use polars::prelude::*;
 use std::collections::HashMap;
-use std::fmt::Display;
-use std::fmt::Formatter;
 use std::fs;
 use std::fs::File;
 use std::str;
 
 mod prql_std;
 mod prql_vm;
+mod type_system;
 
 const TERM_NULL: u16 = 0;
 const TERM_BOOL: u16 = 1;
@@ -70,130 +69,6 @@ struct Cli {
     verbosity: u8,
     #[arg(short, long, default_value_t = String::new())]
     input_file: String,
-}
-
-pub enum PrqlBaseDataType {
-    Null,
-    Bool,
-    Numeric,
-    String,
-}
-// #[derive(Clone, Debug)]
-pub struct PrqlDataType {
-    base: PrqlBaseDataType,
-    size: usize,
-}
-
-impl Default for PrqlDataType {
-    fn default() -> Self {
-        PrqlDataType {
-            base: PrqlBaseDataType::Null,
-            size: 0,
-        }
-    }
-}
-
-impl PrqlDataType {
-    /// Convert to the physical data type
-    #[must_use]
-    pub fn to_physical(&self) -> DataType {
-        match self.base {
-            PrqlBaseDataType::Null => DataType::Null,
-            PrqlBaseDataType::Bool => DataType::Boolean,
-            PrqlBaseDataType::Numeric => DataType::Float64,
-            PrqlBaseDataType::String => DataType::Utf8,
-        }
-    }
-
-    pub fn is_scalar(&self) -> bool {
-        self.size != 1
-    }
-
-    pub fn polars_to_prql(dtype: &DataType, size: usize) -> PrqlDataType {
-        match dtype {
-            DataType::Null => PrqlDataType {
-                base: PrqlBaseDataType::Null,
-                size: size,
-            },
-            DataType::Unknown => PrqlDataType {
-                base: PrqlBaseDataType::Null,
-                size: size,
-            },
-            DataType::Boolean => PrqlDataType {
-                base: PrqlBaseDataType::Bool,
-                size: size,
-            },
-            DataType::UInt8 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::UInt16 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::UInt32 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::UInt64 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Int8 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Int16 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Int32 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Int64 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Float32 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Float64 => PrqlDataType {
-                base: PrqlBaseDataType::Numeric,
-                size: size,
-            },
-            DataType::Utf8 => PrqlDataType {
-                base: PrqlBaseDataType::String,
-                size: size,
-            },
-            _ => PrqlDataType {
-                base: PrqlBaseDataType::Null,
-                size: size,
-            },
-        }
-    }
-
-    pub fn prql_to_polars(self) -> DataType {
-        match self.base {
-            PrqlBaseDataType::Null => DataType::Null,
-            PrqlBaseDataType::Bool => DataType::Boolean,
-            PrqlBaseDataType::Numeric => DataType::Float64,
-            PrqlBaseDataType::String => DataType::Utf8,
-        }
-    }
-}
-
-impl Display for PrqlDataType {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let s = match self.base {
-            PrqlBaseDataType::Null => format!("Null[{}]", self.size),
-            PrqlBaseDataType::Bool => format!("Bool[{}]", self.size),
-            PrqlBaseDataType::Numeric => format!("Numeric[{}]", self.size),
-            PrqlBaseDataType::String => format!("String[{}]", self.size),
-        };
-        f.write_str(s.as_str())
-    }
 }
 
 fn main() {
@@ -1028,7 +903,7 @@ pub fn prql_import(vm: &mut PRQLVirtualMachine) {
             for col in head.schema().iter() {
                 schema.with_column(
                     col.0.to_string(),
-                    PrqlDataType::polars_to_prql(col.1, 0).to_physical(),
+                    type_system::PrqlDataType::polars_to_prql(col.1, 0).to_physical(),
                 );
             }
 
@@ -1042,10 +917,6 @@ pub fn prql_import(vm: &mut PRQLVirtualMachine) {
                 // .with_quote_char(quote)
                 .finish()
                 .unwrap();
-
-            for col in vm.__current_table.schema().iter() {
-                println!("{} {}", col.0, col.1);
-            }
         }
         "json" => {}
         _ => {}
---
+++ prqlvm/src/type_system.rs
@@ -0,0 +1,128 @@
+use polars::prelude::*;
+use std::fmt::Display;
+use std::fmt::Formatter;
+
+pub enum PrqlBaseDataType {
+    Null,
+    Bool,
+    Numeric,
+    String,
+}
+
+// #[derive(Clone, Debug)]
+pub struct PrqlDataType {
+    base: PrqlBaseDataType,
+    size: usize,
+}
+
+impl Default for PrqlDataType {
+    fn default() -> Self {
+        PrqlDataType {
+            base: PrqlBaseDataType::Null,
+            size: 0,
+        }
+    }
+}
+
+impl PrqlDataType {
+    /// Convert to the physical data type
+    #[must_use]
+    pub fn to_physical(&self) -> DataType {
+        match self.base {
+            PrqlBaseDataType::Null => DataType::Null,
+            PrqlBaseDataType::Bool => DataType::Boolean,
+            PrqlBaseDataType::Numeric => DataType::Float64,
+            PrqlBaseDataType::String => DataType::Utf8,
+        }
+    }
+
+    pub fn is_scalar(&self) -> bool {
+        self.size != 1
+    }
+
+    pub fn polars_to_prql(dtype: &DataType, size: usize) -> PrqlDataType {
+        match dtype {
+            DataType::Null => PrqlDataType {
+                base: PrqlBaseDataType::Null,
+                size: size,
+            },
+            DataType::Unknown => PrqlDataType {
+                base: PrqlBaseDataType::Null,
+                size: size,
+            },
+            DataType::Boolean => PrqlDataType {
+                base: PrqlBaseDataType::Bool,
+                size: size,
+            },
+            DataType::UInt8 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::UInt16 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::UInt32 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::UInt64 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Int8 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Int16 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Int32 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Int64 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Float32 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Float64 => PrqlDataType {
+                base: PrqlBaseDataType::Numeric,
+                size: size,
+            },
+            DataType::Utf8 => PrqlDataType {
+                base: PrqlBaseDataType::String,
+                size: size,
+            },
+            _ => PrqlDataType {
+                base: PrqlBaseDataType::Null,
+                size: size,
+            },
+        }
+    }
+
+    pub fn prql_to_polars(self) -> DataType {
+        match self.base {
+            PrqlBaseDataType::Null => DataType::Null,
+            PrqlBaseDataType::Bool => DataType::Boolean,
+            PrqlBaseDataType::Numeric => DataType::Float64,
+            PrqlBaseDataType::String => DataType::Utf8,
+        }
+    }
+}
+
+impl Display for PrqlDataType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s = match self.base {
+            PrqlBaseDataType::Null => format!("Null[{}]", self.size),
+            PrqlBaseDataType::Bool => format!("Bool[{}]", self.size),
+            PrqlBaseDataType::Numeric => format!("Numeric[{}]", self.size),
+            PrqlBaseDataType::String => format!("String[{}]", self.size),
+        };
+        f.write_str(s.as_str())
+    }
+}
