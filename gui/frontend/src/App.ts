1b2b48f957a365824ba6c14677d6a851c004b6b5
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-01-22 12:03:26 +0000

    [gui] App elements code moved from index.htlm to App.ts.

--- gui/frontend/src/App.ts
+++ gui/frontend/src/App.ts
@@ -1,27 +1,72 @@
 import { SavedAppState } from "./declarations";
 import PreludioPipeline from "./PreludioPipeline";
+import { MainButton } from "./utils/MainButton";
 
-class App {
+export class App extends HTMLDivElement {
+  private topMenuPaneElement: HTMLDivElement;
+  private mainPaneElement: HTMLDivElement;
+  private codeEditorPaneElement: HTMLDivElement;
+  private pipelineEditorPaneElement: HTMLDivElement;
+  private tableEditorPaneElement: HTMLDivElement;
+
   private static pipelineCounter: number = 0;
   private pipelines: Array<PreludioPipeline>;
 
   constructor(savedStatus?: SavedAppState) {
+    super();
+    this.id = "app";
+
+    this.topMenuPaneElement = document.createElement("div");
+    this.mainPaneElement = document.createElement("div");
+    this.codeEditorPaneElement = document.createElement("div");
+    this.pipelineEditorPaneElement = document.createElement("div");
+    this.tableEditorPaneElement = document.createElement("div");
+
+    this._initHTMLElement();
+
     this.pipelines = new Array<PreludioPipeline>();
 
     if (savedStatus !== undefined) {
       this._loadSavedStatus(savedStatus);
     }
-    this._initCodeEditorPane();
+  }
+
+  private _initHTMLElement() {
+    this.topMenuPaneElement.id = "top-menu-pane";
+    this.topMenuPaneElement.className = "menu-pane";
+
+    this.mainPaneElement.id = "main-pane";
+
+    // Code Editor Pane
+    this.codeEditorPaneElement.id = "code-editor-pane";
+    this.pipelineEditorPaneElement.id = "pipeline-editor-pane";
+
+    const addNewPipelineButton = new MainButton(
+      "add-new-pipeline",
+      "src/assets/icons/plus-square-svgrepo-com.svg"
+    );
+    addNewPipelineButton.addEventListener("click", () => this.addNewPipeline());
+
+    this.codeEditorPaneElement.appendChild(addNewPipelineButton);
+    this.codeEditorPaneElement.appendChild(this.pipelineEditorPaneElement);
+
+    // Table Editor Pane
+    this.tableEditorPaneElement.id = "table-editor-pane";
+    this.tableEditorPaneElement.appendChild(
+      new MainButton("import-table", "src/assets/icons/import-svgrepo-com.svg")
+    );
+
+    // Main Panel
+    this.mainPaneElement.appendChild(this.codeEditorPaneElement);
+    this.mainPaneElement.appendChild(this.tableEditorPaneElement);
+
+    // App
+    this.appendChild(this.topMenuPaneElement);
+    this.appendChild(this.mainPaneElement);
   }
 
   private _loadSavedStatus(savedStatus: SavedAppState) {
     this.pipelines = savedStatus.pipelines;
-  }
-
-  // Initialize the code editor panel
-  private _initCodeEditorPane() {
-    if (this.pipelines.length === 0) {
-    }
   }
 
   addNewPipeline(pipelineName?: string) {
@@ -48,4 +93,6 @@ class App {
   }
 }
 
-export default App;
+window.customElements.define("app-element", App, {
+  extends: "div",
+});
--- gui/frontend/src/main.ts
+++ gui/frontend/src/main.ts
@@ -1,11 +1,10 @@
 import "./style.css";
-// import "./app.css";
 
-import App from "./App";
-import { CURRENT_DICTIONARY } from "./declarations";
-// import dictionary from "./assets/dictionary.json";
-// import functionsList from "./assets/preludio-functions.json";
-// import { LangDictType, PreludioFunctionsList } from "./declarations";
+import { App } from "./App";
+
+document.body.appendChild(new App());
+
+// Default Wails code
 
 // import { Greet } from "../wailsjs/go/main/App";
 
@@ -32,35 +31,8 @@ import { CURRENT_DICTIONARY } from "./declarations";
 //   }
 // };
 
-// let nameElement = document.getElementById("name") as HTMLInputElement;
-// nameElement.focus();
-// let resultElement = document.getElementById("result");
-
-// export const CURRENT_DICTIONARY: LangDictType = dictionary;
-// export const PRELUDIO_FUNCTIONS_LIST: PreludioFunctionsList = functionsList;
-
-function loadDictionary() {
-  for (let entry of Object.entries(CURRENT_DICTIONARY)) {
-    const e = document.getElementById(entry[0]);
-    if (e !== null) {
-      e.innerHTML = entry[1];
-    }
-  }
-}
-
-loadDictionary();
-
-const app = new App();
-
-const createNewPipelineButton = document.getElementById(
-  "add-new-pipeline-button"
-);
-if (createNewPipelineButton !== null) {
-  createNewPipelineButton.addEventListener("click", () => app.addNewPipeline());
-}
-
-declare global {
-  interface Window {
-    // greet: () => void;
-  }
-}
+// declare global {
+//   interface Window {
+//     greet: () => void;
+//   }
+// }
