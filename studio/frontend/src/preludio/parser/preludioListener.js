68167badb529e8b14e41690fd414b9c07d38b0a7
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-02-16 23:25:49 +0000

    [studio] Parser: format updated.

--- studio/frontend/src/preludio/parser/preludioListener.js
+++ studio/frontend/src/preludio/parser/preludioListener.js
@@ -1,243 +1,162 @@
 // Generated from .\preludio.g4 by ANTLR 4.9.2
 // jshint ignore: start
-import antlr4 from 'antlr4';
+import antlr4 from "antlr4";
 
 // This class defines a complete listener for a parse tree produced by preludioParser.
 export default class preludioListener extends antlr4.tree.ParseTreeListener {
+  // Enter a parse tree produced by preludioParser#nl.
+  enterNl(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#nl.
-	enterNl(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#nl.
+  exitNl(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#nl.
-	exitNl(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#program.
+  enterProgram(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#program.
+  exitProgram(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#program.
-	enterProgram(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#programIntro.
+  enterProgramIntro(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#program.
-	exitProgram(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#programIntro.
+  exitProgramIntro(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#funcDef.
+  enterFuncDef(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#programIntro.
-	enterProgramIntro(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#funcDef.
+  exitFuncDef(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#programIntro.
-	exitProgramIntro(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#funcDefName.
+  enterFuncDefName(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#funcDefName.
+  exitFuncDefName(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#funcDef.
-	enterFuncDef(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#funcDefParams.
+  enterFuncDefParams(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#funcDef.
-	exitFuncDef(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#funcDefParams.
+  exitFuncDefParams(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#funcDefParam.
+  enterFuncDefParam(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#funcDefName.
-	enterFuncDefName(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#funcDefParam.
+  exitFuncDefParam(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#funcDefName.
-	exitFuncDefName(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#typeDef.
+  enterTypeDef(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#typeDef.
+  exitTypeDef(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#funcDefParams.
-	enterFuncDefParams(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#typeTerm.
+  enterTypeTerm(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#funcDefParams.
-	exitFuncDefParams(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#typeTerm.
+  exitTypeTerm(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#stmt.
+  enterStmt(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#funcDefParam.
-	enterFuncDefParam(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#stmt.
+  exitStmt(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#funcDefParam.
-	exitFuncDefParam(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#assignStmt.
+  enterAssignStmt(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#assignStmt.
+  exitAssignStmt(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#typeDef.
-	enterTypeDef(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#pipeline.
+  enterPipeline(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#typeDef.
-	exitTypeDef(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#pipeline.
+  exitPipeline(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#inlinePipeline.
+  enterInlinePipeline(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#typeTerm.
-	enterTypeTerm(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#inlinePipeline.
+  exitInlinePipeline(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#typeTerm.
-	exitTypeTerm(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#identBacktick.
+  enterIdentBacktick(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#identBacktick.
+  exitIdentBacktick(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#stmt.
-	enterStmt(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#funcCall.
+  enterFuncCall(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#stmt.
-	exitStmt(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#funcCall.
+  exitFuncCall(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#funcCallParam.
+  enterFuncCallParam(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#assignStmt.
-	enterAssignStmt(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#funcCallParam.
+  exitFuncCallParam(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#assignStmt.
-	exitAssignStmt(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#namedArg.
+  enterNamedArg(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#namedArg.
+  exitNamedArg(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#pipeline.
-	enterPipeline(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#assign.
+  enterAssign(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#pipeline.
-	exitPipeline(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#assign.
+  exitAssign(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#multiAssign.
+  enterMultiAssign(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#inlinePipeline.
-	enterInlinePipeline(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#multiAssign.
+  exitMultiAssign(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#inlinePipeline.
-	exitInlinePipeline(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#exprCall.
+  enterExprCall(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#exprCall.
+  exitExprCall(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#identBacktick.
-	enterIdentBacktick(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#expr.
+  enterExpr(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#identBacktick.
-	exitIdentBacktick(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#expr.
+  exitExpr(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#term.
+  enterTerm(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#funcCall.
-	enterFuncCall(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#term.
+  exitTerm(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#funcCall.
-	exitFuncCall(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#exprUnary.
+  enterExprUnary(ctx) {}
 
+  // Exit a parse tree produced by preludioParser#exprUnary.
+  exitExprUnary(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#funcCallParam.
-	enterFuncCallParam(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#literal.
+  enterLiteral(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#funcCallParam.
-	exitFuncCallParam(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#literal.
+  exitLiteral(ctx) {}
 
+  // Enter a parse tree produced by preludioParser#list.
+  enterList(ctx) {}
 
-	// Enter a parse tree produced by preludioParser#namedArg.
-	enterNamedArg(ctx) {
-	}
+  // Exit a parse tree produced by preludioParser#list.
+  exitList(ctx) {}
 
-	// Exit a parse tree produced by preludioParser#namedArg.
-	exitNamedArg(ctx) {
-	}
+  // Enter a parse tree produced by preludioParser#nestedPipeline.
+  enterNestedPipeline(ctx) {}
 
-
-	// Enter a parse tree produced by preludioParser#assign.
-	enterAssign(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#assign.
-	exitAssign(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#multiAssign.
-	enterMultiAssign(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#multiAssign.
-	exitMultiAssign(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#exprCall.
-	enterExprCall(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#exprCall.
-	exitExprCall(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#expr.
-	enterExpr(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#expr.
-	exitExpr(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#term.
-	enterTerm(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#term.
-	exitTerm(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#exprUnary.
-	enterExprUnary(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#exprUnary.
-	exitExprUnary(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#literal.
-	enterLiteral(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#literal.
-	exitLiteral(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#list.
-	enterList(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#list.
-	exitList(ctx) {
-	}
-
-
-	// Enter a parse tree produced by preludioParser#nestedPipeline.
-	enterNestedPipeline(ctx) {
-	}
-
-	// Exit a parse tree produced by preludioParser#nestedPipeline.
-	exitNestedPipeline(ctx) {
-	}
-
-
-
+  // Exit a parse tree produced by preludioParser#nestedPipeline.
+  exitNestedPipeline(ctx) {}
 }
