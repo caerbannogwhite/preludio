8c251ac70b7d7cd5b061ca4f572fe48f1ed6208d
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-02-18 15:46:18 +0000

    [preludio] cli updated.

--- cli/go.mod
+++ cli/go.mod
@@ -1,8 +1,10 @@
-module preludio-vm
+module cli
 
 go 1.19
 
-replace preludiovm => ../vm/
+replace preludio => ../preludio/
+
+require preludio v0.0.0-00010101000000-000000000000
 
 require github.com/go-gota/gota v0.12.0 // indirect
 
@@ -11,5 +13,4 @@ require (
 	github.com/alexflint/go-scalar v1.1.0 // indirect
 	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
 	gonum.org/v1/gonum v0.9.1 // indirect
-	preludiovm v1.0.0
 )
--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"preludiovm"
+	"preludio"
 
 	"github.com/alexflint/go-arg"
 )
@@ -18,39 +18,11 @@ func main() {
 	arg.MustParse(&args)
 	// fmt.Println(args.Foo, args.Bar)
 
-	vm := preludiovm.NewPreludioVM(&preludiovm.PreludioVMParams{
-		PrintWarnings: args.Warnings,
-		DebugLevel:    args.DebugLevel,
-		InputPath:     args.InputPath,
-	})
+	vm := new(preludio.ByteEater).
+		InitVM().
+		SetPrintWarning(args.Warnings).
+		SetDebugLevel(args.DebugLevel).
+		SetInputPath(args.InputPath)
 
-	vm.ReadPreludioBytecode()
-
-	// f, _ := os.Open("C:\\Users\\massi\\Downloads\\Cars.csv")
-
-	// cars := dataframe.ReadCSV(f, dataframe.WithDelimiter(';'))
-
-	// switch cars.Col("Car").Type() {
-	// case series.Bool:
-	// 	fmt.Println("it's bool")
-	// case series.Int:
-	// 	fmt.Println("it's int")
-	// case series.String:
-	// 	fmt.Println("it's string")
-	// }
-
-	// mean := func(s series.Series) series.Series {
-	// 	floats := s.Float()
-
-	// 	sum := 0.0
-	// 	for _, f := range floats {
-	// 		sum += f
-	// 	}
-	// 	return series.Floats(sum / float64(len(floats)))
-	// }
-
-	// cars2 := cars.Select(2).Capply(mean)
-	// fmt.Println(cars2)
-
-	// fmt.Printf("%t %T\n", false, true)
+	vm.ReadFileBytecode()
 }
