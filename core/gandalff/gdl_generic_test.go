108d9cc15f267d578a49688125b13040bbe46e1f
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-04-27 23:10:26 +0100

    [gandalff] Gen struct benchmark updated.

--- core/gandalff/gdl_generic_test.go
+++ core/gandalff/gdl_generic_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var SIZE = 100_000
+var SIZE = 1_000_000
 
 type MyInterface interface {
 	Times2() MyInterface
@@ -13,6 +13,8 @@ type MyInterface interface {
 }
 
 type MyStruct struct {
+	b bool
+	s string
 	v []float64
 }
 
@@ -20,6 +22,9 @@ func (s MyStruct) Mod7() MyStruct {
 	for i := range s.v {
 		s.v[i] = math.Mod(s.v[i], 7.0)
 	}
+
+	s.b = true
+	s.s += "Mod7"
 	return s
 }
 
@@ -27,6 +32,9 @@ func (s MyStruct) Times3() MyStruct {
 	for i := range s.v {
 		s.v[i] *= 3
 	}
+
+	s.b = true
+	s.s += "Times3"
 	return s
 }
 
@@ -34,10 +42,15 @@ func (s MyStruct) PlusOne() MyStruct {
 	for i := range s.v {
 		s.v[i] += 1
 	}
+
+	s.b = true
+	s.s += "PlusOne"
 	return s
 }
 
 type MyStructPtr struct {
+	b bool
+	s string
 	v *[]float64
 }
 
@@ -46,6 +59,9 @@ func (s MyStructPtr) Mod7() MyStructPtr {
 	for i := range v {
 		v[i] = math.Mod(v[i], 7.0)
 	}
+
+	s.b = true
+	s.s += "Mod7"
 	s.v = &v
 	return s
 }
@@ -55,6 +71,9 @@ func (s MyStructPtr) Times3() MyStructPtr {
 	for i := range v {
 		v[i] *= 3
 	}
+
+	s.b = true
+	s.s += "Times3"
 	s.v = &v
 	return s
 }
@@ -64,6 +83,9 @@ func (s MyStructPtr) PlusOne() MyStructPtr {
 	for i := range v {
 		v[i] += 1
 	}
+
+	s.b = true
+	s.s += "PlusOne"
 	s.v = &v
 	return s
 }
@@ -76,6 +98,14 @@ func Test_Gen_MyStruct(t *testing.T) {
 	}
 
 	res := (MyStruct{v: v}).Mod7().Times3().PlusOne().Mod7().PlusOne().Times3().PlusOne().Times3().Mod7()
+	if res.b != true {
+		t.Fail()
+	}
+
+	if res.s != "Mod7Times3PlusOneMod7PlusOneTimes3PlusOneTimes3Mod7" {
+		t.Fail()
+	}
+
 	if res.v[0] != 0 || res.v[1] != 6 || res.v[2] != 5 || res.v[3] != 4 || res.v[4] != 3 {
 		t.Fail()
 	}
@@ -89,6 +119,14 @@ func Test_Gen_MyStructPtr(t *testing.T) {
 	}
 
 	res := (MyStructPtr{v: &v}).Mod7().Times3().PlusOne().Mod7().PlusOne().Times3().PlusOne().Times3().Mod7()
+	if res.b != true {
+		t.Fail()
+	}
+
+	if res.s != "Mod7Times3PlusOneMod7PlusOneTimes3PlusOneTimes3Mod7" {
+		t.Fail()
+	}
+
 	if (*res.v)[0] != 0 || (*res.v)[1] != 6 || (*res.v)[2] != 5 || (*res.v)[3] != 4 || (*res.v)[4] != 3 {
 		t.Fail()
 	}
