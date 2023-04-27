569cd7a94310e0a666df0985ba61dfc6eb0b8d72
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-04-27 22:51:56 +0100

    [gandalff] Gen struct benchmark added.

--- core/gandalff/gdl_generic_test.go
+++ core/gandalff/gdl_generic_test.go
@@ -1,9 +1,11 @@
 package gandalff
 
 import (
-	"fmt"
+	"math"
 	"testing"
 )
+
+var SIZE = 100_000
 
 type MyInterface interface {
 	Times2() MyInterface
@@ -14,9 +16,16 @@ type MyStruct struct {
 	v []float64
 }
 
-func (s MyStruct) Times2() MyStruct {
+func (s MyStruct) Mod7() MyStruct {
 	for i := range s.v {
-		s.v[i] *= 2
+		s.v[i] = math.Mod(s.v[i], 7.0)
+	}
+	return s
+}
+
+func (s MyStruct) Times3() MyStruct {
+	for i := range s.v {
+		s.v[i] *= 3
 	}
 	return s
 }
@@ -32,58 +41,81 @@ type MyStructPtr struct {
 	v *[]float64
 }
 
-func (s MyStructPtr) Times2() MyStructPtr {
-	for i := range *s.v {
-		(*s.v)[i] *= 2
+func (s MyStructPtr) Mod7() MyStructPtr {
+	v := *s.v
+	for i := range v {
+		v[i] = math.Mod(v[i], 7.0)
 	}
+	s.v = &v
+	return s
+}
+
+func (s MyStructPtr) Times3() MyStructPtr {
+	v := *s.v
+	for i := range v {
+		v[i] *= 3
+	}
+	s.v = &v
 	return s
 }
 
 func (s MyStructPtr) PlusOne() MyStructPtr {
-	for i := range *s.v {
-		(*s.v)[i] += 1
+	v := *s.v
+	for i := range v {
+		v[i] += 1
 	}
+	s.v = &v
 	return s
 }
 
-func Benchmark_Gen_MyStruct(b *testing.B) {
+func Test_Gen_MyStruct(t *testing.T) {
+	v := make([]float64, SIZE)
 
-	N := 100_000
-	v := make([]float64, N)
-
-	for i := 0; i < N; i++ {
+	for i := 0; i < SIZE; i++ {
 		v[i] = float64(i)
 	}
 
-	s := MyStruct{v: v}
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		s.Times2().PlusOne()
+	res := (MyStruct{v: v}).Mod7().Times3().PlusOne().Mod7().PlusOne().Times3().PlusOne().Times3().Mod7()
+	if res.v[0] != 0 || res.v[1] != 6 || res.v[2] != 5 || res.v[3] != 4 || res.v[4] != 3 {
+		t.Fail()
 	}
-	b.StopTimer()
+}
 
-	res := s.Times2().PlusOne()
-	fmt.Println(res.v[0], res.v[1], res.v[2], res.v[3], res.v[4])
+func Test_Gen_MyStructPtr(t *testing.T) {
+	v := make([]float64, SIZE)
+
+	for i := 0; i < SIZE; i++ {
+		v[i] = float64(i)
+	}
+
+	res := (MyStructPtr{v: &v}).Mod7().Times3().PlusOne().Mod7().PlusOne().Times3().PlusOne().Times3().Mod7()
+	if (*res.v)[0] != 0 || (*res.v)[1] != 6 || (*res.v)[2] != 5 || (*res.v)[3] != 4 || (*res.v)[4] != 3 {
+		t.Fail()
+	}
+}
+
+func Benchmark_Gen_MyStruct(b *testing.B) {
+	v := make([]float64, SIZE)
+
+	for i := 0; i < SIZE; i++ {
+		v[i] = float64(i)
+	}
+
+	// b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		(MyStruct{v: v}).Mod7().Times3().PlusOne().Mod7().PlusOne().Times3().PlusOne().Times3().Mod7()
+	}
 }
 
 func Benchmark_Gen_MyStructPtr(b *testing.B) {
+	v := make([]float64, SIZE)
 
-	N := 100_000
-	v := make([]float64, N)
-
-	for i := 0; i < N; i++ {
+	for i := 0; i < SIZE; i++ {
 		v[i] = float64(i)
 	}
 
-	s := MyStructPtr{v: &v}
-
-	b.ResetTimer()
+	// b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		s.Times2().PlusOne()
+		(MyStructPtr{v: &v}).Mod7().Times3().PlusOne().Mod7().PlusOne().Times3().PlusOne().Times3().Mod7()
 	}
-	b.StopTimer()
-
-	res := s.Times2().PlusOne()
-	fmt.Println((*res.v)[0], (*res.v)[1], (*res.v)[2], (*res.v)[3], (*res.v)[4])
 }
