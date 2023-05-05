2bd875a8504e6016d083017fc7f771404918a4a7
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-05-05 16:28:29 +0100

    [gandalff] Benchmarking: added cases for grouping.

--- core/gandalff/benchmarking/pandas_filter.py
+++ core/gandalff/benchmarking/pandas_filter.py
@@ -15,38 +15,42 @@ solution = "pandas"
 fun = ".filter"
 cache = "TRUE"
 on_disk = "FALSE"
+data_names = ["G1_1e5_1e2_0_0", "G1_1e6_1e2_0_0", "G1_1e7_1e2_0_0"]
 
-###################     1e5
+for data_name in data_names:
+    filepath = os.path.join("..", "testdata", data_name+".csv")
+    print("loading dataset %s" % data_name, flush=True)
 
-data_name = "G1_1e5_1e2_0_0"
-filepath = os.path.join("..", "testdata", data_name+".csv")
-print("loading dataset %s" % data_name, flush=True)
+    x = pd.read_csv(filepath)
+    x['id1'] = x['id1'].astype('category') # remove after datatable#1691
+    x['id2'] = x['id2'].astype('category')
+    x['id3'] = x['id3'].astype('category')
+    x['id4'] = x['id4'].astype('Int32') ## NA-aware types improved after h2oai/datatable#2761 resolved
+    x['id5'] = x['id5'].astype('Int32')
+    x['id6'] = x['id6'].astype('Int32')
+    x['v1'] = x['v1'].astype('Int32')
+    x['v2'] = x['v2'].astype('Int32')
+    x['v3'] = x['v3'].astype('float64')
 
-x = pd.read_csv(filepath)
-x['id1'] = x['id1'].astype('category') # remove after datatable#1691
-x['id2'] = x['id2'].astype('category')
-x['id3'] = x['id3'].astype('category')
-x['id4'] = x['id4'].astype('Int32') ## NA-aware types improved after h2oai/datatable#2761 resolved
-x['id5'] = x['id5'].astype('Int32')
-x['id6'] = x['id6'].astype('Int32')
-x['v1'] = x['v1'].astype('Int32')
-x['v2'] = x['v2'].astype('Int32')
-x['v3'] = x['v3'].astype('float64')
+    print(len(x.index), flush=True)
 
-print(len(x.index), flush=True)
 
-question = "filtering 1e5" # q1
-gc.collect()
-times = timeit.repeat(lambda: x[x.id6 > 500][x.v3 < 50][(x.id1 == "id024") | (x.id2 == "id024")][(x.v1 == 5) & (x.v2 == 1)], repeat=5, number=10)
-t = mean(times)
+    ###################     QUESTION 1   ###################
 
-gc.collect()
-ans = x[x.id6 > 500][x.v3 < 50][(x.id1 == "id024") | (x.id2 == "id024")][(x.v1 == 5) & (x.v2 == 1)]
-print(ans.shape, flush=True)
-m = memory_usage()
-chk = [ans['v1'].sum()]
+    question = "filtering: " # q1
+    gc.collect()
+    times = timeit.repeat(lambda: x[x.id6 > 500][x.v3 < 50][(x.id1 == "id024") | (x.id2 == "id024")][(x.v1 == 5) & (x.v2 == 1)], repeat=5, number=10)
+    t = mean(times)
 
-write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=-1, on_disk=on_disk)
-del ans
+    gc.collect()
+    ans = x[x.id6 > 500][x.v3 < 50][(x.id1 == "id024") | (x.id2 == "id024")][(x.v1 == 5) & (x.v2 == 1)]
+    print(ans.shape, flush=True)
+    m = memory_usage()
+    chk = [ans['v1'].sum()]
 
-###################     1e6
+    write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=-1, on_disk=on_disk)
+    del ans
+
+
+    ###################     QUESTION 2   ###################
+
