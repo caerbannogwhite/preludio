6aa02a16f61527e920672180a0a52771eb1e2c89
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-05-06 10:38:31 +0100

    [gandalff] GroupBy benchmark: question 2 added.

--- core/gandalff/gdl_dataframe_bench_test.go
+++ core/gandalff/gdl_dataframe_bench_test.go
@@ -248,3 +248,54 @@ func Benchmark_GroupBy_Q1_1e7(b *testing.B) {
 	}
 	b.StopTimer()
 }
+
+func Benchmark_GroupBy_Q2_1e5(b *testing.B) {
+	f, err := os.OpenFile("testdata\\G1_1e5_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		b.Error(err)
+	}
+	df := FromCSV(f, ',', true, 100)
+	f.Close()
+
+	runtime.GC()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		df.GroupBy("id1", "id2").Sum().Select("id1", "v1")
+	}
+	b.StopTimer()
+}
+
+func Benchmark_GroupBy_Q2_1e6(b *testing.B) {
+	f, err := os.OpenFile("testdata\\G1_1e6_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		b.Error(err)
+	}
+	df := FromCSV(f, ',', true, 100)
+	f.Close()
+
+	runtime.GC()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		df.GroupBy("id1", "id2").Sum().Select("id1", "v1")
+	}
+	b.StopTimer()
+}
+
+func Benchmark_GroupBy_Q2_1e7(b *testing.B) {
+	f, err := os.OpenFile("testdata\\G1_1e7_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		b.Error(err)
+	}
+	df := FromCSV(f, ',', true, 100)
+	f.Close()
+
+	runtime.GC()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		df.GroupBy("id1", "id2").Sum().Select("id1", "v1")
+	}
+	b.StopTimer()
+}
