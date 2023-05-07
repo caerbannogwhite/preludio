01b865537d6d4c63ea234a6fb87531b2ae7a0e2d
massimo.meneghello <massimo.meneghello@quantics.co.uk>
2023-05-08 00:22:20 +0100

    [gandalff] Benchmarking h2oai: tests for GroupBy Q4 added.

--- core/gandalff/gdl_dataframe_base_bench_test.go
+++ core/gandalff/gdl_dataframe_base_bench_test.go
@@ -228,7 +228,7 @@ func Benchmark_Filter_Q2_1e7(b *testing.B) {
 	b.StopTimer()
 }
 
-////////////////////////			GROUP BY
+////////////////////////			GROUP BY TESTS
 //
 // GroupBy challege: more info here https://github.com/h2oai/db-benchmark/tree/master
 
@@ -575,6 +575,160 @@ func Test_GroupBy_Q3_1e7(t *testing.T) {
 		t.Errorf("Expected 4999719.62234443, got %f", check2)
 	}
 }
+
+func Test_GroupBy_Q4_1e4(t *testing.T) {
+	f, err := os.OpenFile("testdata\\G1_1e4_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Skip(err)
+	}
+
+	df := NewBaseDataFrame().
+		FromCSV().
+		SetDelimiter(',').
+		SetReader(f).
+		Read().
+		GroupBy("id4").
+		Agg(Mean("v1"), Mean("v2"), Mean("v3"))
+
+	if df.NRows() != 100 {
+		t.Errorf("Expected 100 rows, got %d", df.NRows())
+	}
+
+	if df.NCols() != 4 {
+		t.Errorf("Expected 4 columns, got %d", df.NCols())
+	}
+
+	check1 := df.Agg(Sum("v1")).Series("v1").Get(0).(float64)
+	if !equalFloats(check1, 300.1460223942026, 10e-6) {
+		t.Errorf("Expected 300.1460223942026, got %f", check1)
+	}
+
+	check2 := df.Agg(Sum("v2")).Series("v2").Get(0).(float64)
+	if !equalFloats(check2, 803.8206781360852, 10e-6) {
+		t.Errorf("Expected 803.8206781360852, got %f", check2)
+	}
+
+	check3 := df.Agg(Sum("v3")).Series("v3").Get(0).(float64)
+	if !equalFloats(check3, 5008.9079567058325, 10e-6) {
+		t.Errorf("Expected 5008.9079567058325, got %f", check3)
+	}
+}
+
+func Test_GroupBy_Q4_1e5(t *testing.T) {
+	f, err := os.OpenFile("testdata\\G1_1e5_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Skip(err)
+	}
+
+	df := NewBaseDataFrame().
+		FromCSV().
+		SetDelimiter(',').
+		SetReader(f).
+		Read().
+		GroupBy("id4").
+		Agg(Mean("v1"), Mean("v2"), Mean("v3"))
+
+	if df.NRows() != 100 {
+		t.Errorf("Expected 100 rows, got %d", df.NRows())
+	}
+
+	if df.NCols() != 4 {
+		t.Errorf("Expected 4 columns, got %d", df.NCols())
+	}
+
+	check1 := df.Agg(Sum("v1")).Series("v1").Get(0).(float64)
+	if !equalFloats(check1, 300.29996127903826, 10e-6) {
+		t.Errorf("Expected 300.29996127903826, got %f", check1)
+	}
+
+	check2 := df.Agg(Sum("v2")).Series("v2").Get(0).(float64)
+	if !equalFloats(check2, 800.8632014058803, 10e-6) {
+		t.Errorf("Expected 800.8632014058803, got %f", check2)
+	}
+
+	check3 := df.Agg(Sum("v3")).Series("v3").Get(0).(float64)
+	if !equalFloats(check3, 5009.03811345283, 10e-6) {
+		t.Errorf("Expected 5009.03811345283, got %f", check3)
+	}
+}
+
+func Test_GroupBy_Q4_1e6(t *testing.T) {
+	f, err := os.OpenFile("testdata\\G1_1e6_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Skip(err)
+	}
+
+	df := NewBaseDataFrame().
+		FromCSV().
+		SetDelimiter(',').
+		SetReader(f).
+		Read().
+		GroupBy("id4").
+		Agg(Mean("v1"), Mean("v2"), Mean("v3"))
+
+	if df.NRows() != 100 {
+		t.Errorf("Expected 100 rows, got %d", df.NRows())
+	}
+
+	if df.NCols() != 4 {
+		t.Errorf("Expected 4 columns, got %d", df.NCols())
+	}
+
+	check1 := df.Agg(Sum("v1")).Series("v1").Get(0).(float64)
+	if !equalFloats(check1, 300.0300474405866, 10e-6) {
+		t.Errorf("Expected 300.0300474405866, got %f", check1)
+	}
+
+	check2 := df.Agg(Sum("v2")).Series("v2").Get(0).(float64)
+	if !equalFloats(check2, 799.8113837581368, 10e-6) {
+		t.Errorf("Expected 799.8113837581368, got %f", check2)
+	}
+
+	check3 := df.Agg(Sum("v3")).Series("v3").Get(0).(float64)
+	if !equalFloats(check3, 5003.666447664572, 10e-6) {
+		t.Errorf("Expected 5003.666447664572, got %f", check3)
+	}
+}
+
+func Test_GroupBy_Q4_1e7(t *testing.T) {
+	f, err := os.OpenFile("testdata\\G1_1e7_1e2_0_0.csv", os.O_RDONLY, 0666)
+	if err != nil {
+		t.Skip(err)
+	}
+
+	df := NewBaseDataFrame().
+		FromCSV().
+		SetDelimiter(',').
+		SetReader(f).
+		Read().
+		GroupBy("id4").
+		Agg(Mean("v1"), Mean("v2"), Mean("v3"))
+
+	if df.NRows() != 100 {
+		t.Errorf("Expected 100 rows, got %d", df.NRows())
+	}
+
+	if df.NCols() != 4 {
+		t.Errorf("Expected 4 columns, got %d", df.NCols())
+	}
+
+	check1 := df.Agg(Sum("v1")).Series("v1").Get(0).(float64)
+	if !equalFloats(check1, 299.9879818750654, 10e-6) {
+		t.Errorf("Expected 299.9879818750654, got %f", check1)
+	}
+
+	check2 := df.Agg(Sum("v2")).Series("v2").Get(0).(float64)
+	if !equalFloats(check2, 799.8941794099782, 10e-6) {
+		t.Errorf("Expected 799.8941794099782, got %f", check2)
+	}
+
+	check3 := df.Agg(Sum("v3")).Series("v3").Get(0).(float64)
+	if !equalFloats(check3, 4999.766872833688, 10e-6) {
+		t.Errorf("Expected 4999.766872833688, got %f", check3)
+	}
+}
+
+////////////////////////			GROUP BY BENCHMARKS
 
 func Benchmark_GroupBy_Q1_1e4(b *testing.B) {
 	f, err := os.OpenFile("testdata\\G1_1e4_1e2_0_0.csv", os.O_RDONLY, 0666)
