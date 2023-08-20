c62118ad680d8bc8daa4239d7aa46341c2f5bcb3
caerbannogwhite <massimo.meneghello93@gmail.com>
2023-08-20 12:08:38 +0100

    [Core] Operators: code simplyfied.

--- core/operators.go
+++ core/operators.go
@@ -6,7 +6,7 @@ import (
 	"typesys"
 )
 
-func solveExpr(vm *ByteEater, i *__p_intern__) error {
+func (vm *ByteEater) solveExpr(i *__p_intern__) error {
 	// TODO: check if this is possible and
 	// if it's the case to raise an error
 	if i == nil || i.expr == nil || len(i.expr) == 0 {
@@ -23,7 +23,7 @@ func solveExpr(vm *ByteEater, i *__p_intern__) error {
 
 		case __p_list__:
 			for idx := range l {
-				if err := solveExpr(vm, &l[idx]); err != nil {
+				if err := vm.solveExpr(&l[idx]); err != nil {
 					return err
 				}
 			}
@@ -34,11 +34,15 @@ func solveExpr(vm *ByteEater, i *__p_intern__) error {
 
 	stack := make([]interface{}, 0)
 
+	var iterations int
+	var op typesys.OPCODE
+	var ok, errorMode bool
+	var result interface{}
+
 	for len(i.expr) > 1 {
 
 		// Load the stack until we find an operators
-		var ok bool
-		var op typesys.OPCODE
+		iterations = 0
 		for {
 			if op, ok = isOperator(i.expr[0]); ok {
 				i.expr = i.expr[1:len(i.expr)]
@@ -47,22 +51,50 @@ func solveExpr(vm *ByteEater, i *__p_intern__) error {
 
 			stack = append(stack, i.expr[0])
 			i.expr = i.expr[1:len(i.expr)]
+
+			iterations++
 		}
 
-		var result interface{}
+		errorMode = false
+		result = gandalff.SeriesError{}
 
 		// UNARY
-		// if op, ok := isOperator(t2); ok {
-		// 	i.expr = i.expr[2:len(i.expr)]
+		// if iterations == 1 {
+		// 	t1 := stack[len(stack)-1]
+		// 	stack = stack[0 : len(stack)-1]
 
 		// 	if s, ok := t1.(__p_symbol__); ok {
 		// 		t1 = vm.symbolResolution(s)
 		// 	}
 
 		// 	switch op {
-		// 	case typesys.OP_UNARY_ADD:
+		// 	// case typesys.OP_UNARY_ADD: // TODO: ignore?
+
 		// 	case typesys.OP_UNARY_SUB:
+		// 		switch s1 := t1.(type) {
+		// 		case gandalff.SeriesInt32:
+		// 			result = s1.Neg()
+		// 		case gandalff.SeriesInt64:
+		// 			result = s1.Neg()
+		// 		case gandalff.SeriesFloat64:
+		// 			result = s1.Neg()
+		// 		default:
+		// 			errorMode = true
+		// 		}
+
 		// 	case typesys.OP_UNARY_NOT:
+		// 		if s1, ok := t1.(gandalff.SeriesBool); ok {
+		// 			result = s1.Not()
+		// 		} else {
+		// 			errorMode = true
+		// 		}
+		// 	}
+
+		// 	// Check for errors
+		// 	if _, ok := result.(gandalff.SeriesError); ok || errorMode {
+		// 		return fmt.Errorf("unary operator %s not supported for %s",
+		// 			operatorToString(op),
+		// 			t1.(gandalff.Series).TypeCard().ToString())
 		// 	}
 		// } else
 
@@ -81,83 +113,68 @@ func solveExpr(vm *ByteEater, i *__p_intern__) error {
 				t2 = vm.symbolResolution(s)
 			}
 
+			// Type check
+			s1 := t1.(gandalff.Series)
+			s2 := t2.(gandalff.Series)
+
 			switch op {
 			case typesys.OP_BINARY_MUL:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Mul(t2.(gandalff.Series))
-				}
+				result = s1.Mul(s2)
 
 			case typesys.OP_BINARY_DIV:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Div(t2.(gandalff.Series))
-				}
+				result = s1.Div(s2)
 
 			case typesys.OP_BINARY_MOD:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Mod(t2.(gandalff.Series))
-				}
+				result = s1.Mod(s2)
 
 			case typesys.OP_BINARY_POW:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Pow(t2.(gandalff.Series))
-				}
+				result = s1.Pow(s2)
 
 			case typesys.OP_BINARY_ADD:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Add(t2.(gandalff.Series))
-				}
+				result = s1.Add(s2)
 
 			case typesys.OP_BINARY_SUB:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Sub(t2.(gandalff.Series))
-				}
+				result = s1.Sub(s2)
+
+			case typesys.OP_BINARY_EQ:
+				result = s1.Eq(s2)
+
+			case typesys.OP_BINARY_NE:
+				result = s1.Ne(s2)
+
+			case typesys.OP_BINARY_LT:
+				result = s1.Lt(s2)
+
+			case typesys.OP_BINARY_LE:
+				result = s1.Le(s2)
+
+			case typesys.OP_BINARY_GT:
+				result = s1.Gt(s2)
+
+			case typesys.OP_BINARY_GE:
+				result = s1.Ge(s2)
 
 			case typesys.OP_BINARY_AND:
 				if s1, ok := t1.(gandalff.SeriesBool); ok {
-					result = s1.And(t2.(gandalff.Series))
+					result = s1.And(s2)
+				} else {
+					errorMode = true
 				}
 
 			case typesys.OP_BINARY_OR:
 				if s1, ok := t1.(gandalff.SeriesBool); ok {
-					result = s1.Or(t2.(gandalff.Series))
-				}
-
-			case typesys.OP_BINARY_EQ:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Eq(t2.(gandalff.Series))
-				}
-
-			case typesys.OP_BINARY_NE:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Ne(t2.(gandalff.Series))
-				}
-
-			case typesys.OP_BINARY_LT:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Lt(t2.(gandalff.Series))
-				}
-
-			case typesys.OP_BINARY_LE:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Le(t2.(gandalff.Series))
-				}
-
-			case typesys.OP_BINARY_GT:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Gt(t2.(gandalff.Series))
-				}
-
-			case typesys.OP_BINARY_GE:
-				if s1, ok := t1.(gandalff.Series); ok {
-					result = s1.Ge(t2.(gandalff.Series))
+					result = s1.Or(s2)
+				} else {
+					errorMode = true
 				}
 			}
 
-			if _, ok := result.(gandalff.SeriesError); ok {
-				return fmt.Errorf("operator %s not supported between %s and %s",
+			// Check for errors
+			if _, ok := result.(gandalff.SeriesError); ok || errorMode {
+				return fmt.Errorf("binary operator %s not supported between %s and %s",
 					operatorToString(op),
-					t1.(gandalff.Series).TypeCard().ToString(),
-					t2.(gandalff.Series).TypeCard().ToString())
+					s1.TypeCard().ToString(),
+					s2.TypeCard().ToString())
 			}
 		}
 
