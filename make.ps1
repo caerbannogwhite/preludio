1aec1eb811d636bb81b68d615291e47f55d92db2
caerbannogwhite <massimo.meneghello93@gmail.com>
2023-09-29 23:58:20 +0100

    [grammar] make script updated: debug code.

--- make.ps1
+++ make.ps1
@@ -1,3 +1,8 @@
+# DEBUG SCRIPT
+# Remove-Item .\.antlr\*
+# antlr.bat preludioLexer.g4 preludioParser.g4 -o .\.antlr
+# javac.exe .\.antlr\*.java
+
 antlr.bat -listener -no-visitor -Dlanguage=Go -package bytefeeder preludioLexer.g4 preludioParser.g4
 
 Move-Item -force .\preludio*.go .\core\bytefeeder\
